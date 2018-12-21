020878e4a61cb12fb66d937008c67ce28b31da15
hp <namstudionsk@gmail.com>
2018-12-21 22:52:05 +0700

    Fix

--- src/main/java/com/example/demo/FooController.java
+++ src/main/java/com/example/demo/FooController.java
@@ -9,7 +9,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class FooController {
     @GetMapping("/")
     public String index(){
-        return "<h1>Hello World!</h1>";
+        return "<h1>Hello World!</h1><br />" +
+                "<a href=\"/create\">Create</a><br />" +
+                "<a href=\"/retrieve\">Retrieve</a><br />" +
+                "<a href=\"/update\">Update</a><br />" +
+                "<a href=\"/delete\">Delete</a>";
+    }
+
+    @GetMapping("/create")
+    public String indexCreate(){
+        return "<h1>Create</h1>";
+    }
+
+    @GetMapping("/retrieve")
+    public String indexRetrieve(){
+        return "<h1>Retrieve</h1>";
+    }
+
+    @GetMapping("/update")
+    public String indexUpdate(){
+        return "<h1>Update</h1>";
+    }
+
+    @GetMapping("/delete")
+    public String indexDelete(){
+        return "<h1>Delete</h1>";
     }
 
     @RequestMapping("/42")
