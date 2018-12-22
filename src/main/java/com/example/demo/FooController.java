7f2cd4cf3e041ea8e63559776566edc7aa594ed0
hp <namstudionsk@gmail.com>
2018-12-22 12:20:38 +0700

    Fix Controller

--- src/main/java/com/example/demo/FooController.java
+++ src/main/java/com/example/demo/FooController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FooController {
+
+    private DataDAOImpl dataDAO;
+
     @GetMapping("/")
     public String index(){
+        System.out.println("4111111111111111111111112");
         return "<h1>Hello World!</h1><br />" +
                 "<a href=\"/create\">Create</a><br />" +
                 "<a href=\"/retrieve\">Retrieve</a><br />" +
@@ -18,16 +22,28 @@ public class FooController {
 
     @GetMapping("/create")
     public String indexCreate(){
+        dataDAO = new DataDAOImpl();
+        System.out.println("4242");
         return "<h1>Create</h1>";
     }
 
     @GetMapping("/retrieve")
     public String indexRetrieve(){
-        return "<h1>Retrieve</h1>";
+        StringBuilder str = new StringBuilder();
+
+        for (Data data : dataDAO.getAllData()){
+            str.append("Data : [Id : " + data.getId() + ", Name : " + data.getName() + " ]<br />");
+        }
+
+        return "<h1>Retrieve</h1><br />" + str.toString();
     }
 
     @GetMapping("/update")
     public String indexUpdate(){
+        Data data = dataDAO.getAllData().get(0);
+        data.setName("Michael");
+        dataDAO.updateData(data);
+
         return "<h1>Update</h1>";
     }
 
@@ -38,6 +54,10 @@ public class FooController {
 
     @RequestMapping("/42")
     public String index42(@RequestParam(value = "name", defaultValue = "42") String name){
+        Data data = dataDAO.getAllData().get(0);
+        data.setName(name);
+        dataDAO.updateData(data);
+
         return String.format("<h2>%s</h2>", name);
     }
 }
